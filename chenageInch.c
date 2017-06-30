f7655acacfa0d817ca220df056434254c0dea57e
Susumu Kanai <kanai.susumu@meta.co.jp>
2017-06-30 16:46:58 +0900

    new add
    Nee add

---
+++ chenageInch.c
@@ -0,0 +1,21 @@
+/********************************/
+/*  センチメートル→インチ変換  */
+/********************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void  main( )
+{
+    char    c[128];
+    double  lc,li;
+
+    printf("\n***** 長さの単位変換 *****");
+    printf("\n（センチメートル→インチ）");
+    printf("\n\n長さ(cm)： "); fflush(stdout);
+
+    gets(c); lc=atof(c);                 // 文字列入力，数値に変換
+
+    li=lc/2.54;
+
+    printf("\n  %f cm → %f インチ\n",lc,li);
+}
